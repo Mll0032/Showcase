934806660fcb2ee3d95f456c7e05c11820b3d8fa
Mll0032 <mllawson18@gmail.com>
2025-01-24 23:42:53 -0600

    Add css styling to suggestion drop down menu

--- game-night-nexus/vite.config.js
+++ game-night-nexus/vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Your backend server
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:3001',
+      
     },
   },
 });
